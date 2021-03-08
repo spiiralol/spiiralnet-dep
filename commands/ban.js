94f0e6be246f8c9cf6c4eaefc478cb17d07b4009
spiiralol <henderson.finlay.0@icloud.com>
2021-03-08 23:18:23 +0000

    Update ban.js & kick.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,13 +1,25 @@
 module.exports = {
     name: 'ban',
     description: 'Bans a member.',
-    execute(client, message, args) {
+    async execute(client, message, args) {
         if (message.member.permissions.has("BAN_MEMBERS")) {
             const target = message.mentions.users.first();
             if(target) {
+                const delay = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
                 const memberTarget = message.guild.members.cache.get(target.id);
-                memberTarget.ban({days: args[1], reason: args[2]});
-                message.channel.send('Member has been banned.')
+                
+                const time = args[1]
+                const reason = args.slice(2).join(" ")
+
+                const kickEmbed = new Discord.MessageEmbed()
+                    .setTitle('Ban Report')
+                    .setAuthor(`Moderator: ${message.author.tag}`)
+                    .setDescription(`You have been banned from **${message.guild.name}** with reason of ${reason}.`)
+  
+                memberTarget.send(kickEmbed)
+                await delay(100);
+                memberTarget.kick(reason)
+                message.channel.send('Member has been kicked.')
             } else {
                 message.channel.send('Unable to find member.')
             }
--- commands/kick.js
+++ commands/kick.js
@@ -1,12 +1,22 @@
 module.exports = {
     name: 'kick',
     description: 'Kicks a member.',
-    execute(client, message, args) {
+    async execute(client, message, args) {
         if (message.member.permissions.has("KICK_MEMBERS")) {
             const target = message.mentions.users.first();
             if(target) {
+                const delay = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
                 const memberTarget = message.guild.members.cache.get(target.id);
-                memberTarget.kick(args.join(" "))
+                const reason = args.slice(1).join(" ")
+
+                const kickEmbed = new Discord.MessageEmbed()
+                    .setTitle('Kick Report')
+                    .setAuthor(`Moderator: ${message.author.tag}`)
+                    .setDescription(`You have been kicked from **${message.guild.name}** with reason of ${reason}.`)
+  
+                memberTarget.send(kickEmbed)
+                await delay(100);
+                memberTarget.kick(reason)
                 message.channel.send('Member has been kicked.')
             } else {
                 message.channel.send('Unable to find member.')
