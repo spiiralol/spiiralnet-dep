fd03b81743b2b47ad59813e6ddf3e4463067ce5d
spiiralol <henderson.finlay.0@icloud.com>
2021-03-08 22:45:56 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,16 @@
 module.exports = {
     name: 'ping',
     description: 'This is a ping command',
-    execute(client, message, args) {
-        message.channel.send('Pong!')
+    execute(client, message, args, Discord) {
+        const pingEmbed = new Discord.MessageEmbed()
+            .setTitle('Discord Latency')
+            .addFields(
+                { name: 'Bot Latency', value: `${Date.now() - message.createdTimestamp}ms`, inline: true },
+                { name: 'API Latency', value: `${Math.round(client.ws.ping)}ms`, inline: true }
+            )
+            .setTimestamp()
+            .setFooter('SpiiralNet')
+
+        message.channel.send(pingEmbed)
     }
 }
