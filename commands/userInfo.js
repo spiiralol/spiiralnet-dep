965839ad5f4388aa5c396ff174228b5d38fbf9a7
spiiralol <henderson.finlay.0@icloud.com>
2021-03-11 13:21:20 +0000

    Update userInfo.js

--- commands/userInfo.js
+++ commands/userInfo.js
@@ -4,20 +4,36 @@ module.exports = {
     name: 'userinfo',
     description: 'Displays info about a user.',
     execute(client, message, args, Discord) {
+        const target = message.mentions.users.first();
+        
         if (!args[0]) {
             const memberTarget = message.author
-            const memberemb = message.guild.members.fetch(memberTarget)
+
+            console.log(message.author)
 
             const userInfoEmbed = new Discord.MessageEmbed()
                 .setAuthor(message.author.tag, message.author.displayAvatarURL())
                 .addField('User ID', message.author.id, true)
                 .addField('Nickname', `${message.author.nickname ? `${message.author.nickname}` : 'None'}`, true)
                 .addField('Joined On', `${moment(message.author.joinedAt).format('DD-MM-YYYY')}`, true)
-                .addField('Created On', `${moment(message.author.createdAt).format('DD-MM-YYYY')}`, true)
+                .addField('Created On', `${moment(memberTarget.createdAt).format('DD-MM-YYYY')}`, true)
                 .setTimestamp()
                 .setFooter('SpiiralNet')
 
             return message.channel.send(userInfoEmbed)
+        } else if (target) {
+            const memberTarget = message.guild.members.cache.get(target.id);
+
+            const userTargetEmbed = new Discord.MessageEmbed()
+                .setAuthor(`${memberTarget.user.username}#${memberTarget.user.discriminator}`, memberTarget.user.displayAvatarURL())
+                .addField('User ID', memberTarget.user.id, true)
+                .addField('Nickname', `${memberTarget.nickname ? `${memberTarget.nickname}` : 'None'}`, true)
+                .addField('Joined On', `${moment(memberTarget.joinedAt).format('DD-MM-YYYY')}`, true)
+                //.addField('Created On', `${moment(message.createdAt).format('DD-MM-YYYY')}`, true)
+                .setTimestamp()
+                .setFooter('SpiiralNet')
+
+            message.channel.send(userTargetEmbed)
         }
     }
 }
